5d9d1c2a68dc565a863c51d491c18458c634f953
Antony Sotirov <antony@sotirov.io>
2024-06-19 00:35:15 +0200

    initial commit; added 3 new settings; updated dice table to use Handlebars

--- README.md
+++ README.md
@@ -1,18 +1,11 @@
-[![Github All Releases](https://img.shields.io/github/downloads/jopeek/fvtt-simple-dice-roller/total.svg)]() [![Donate](https://img.shields.io/badge/Donate-BuyMeACoffee-green.svg)](https://www.buymeacoffee.com/ChalkOne)
-# FVTT - Simple Dice Roller
+# FVTT - Simple Dice Roller Deluxe
 
-Simple module that allows for quick rolling of common dice.
-
-![demo_token_icons](https://thumbs.gfycat.com/ReadySkeletalAnteater-size_restricted.gif)
+A system agnostic module that allows for quick rolling of common dice.
+Heavily inspired and based on the work of JoPeek.
+See original module here: https://github.com/jopeek/fvtt-simple-dice-roller/
 
 ### Installation Instructions
 
-To install a module, follow these instructions:
+To install this module, follow the instructions below:
 
-1. Start FVTT and browse to the Game Modules tab in the Configuration and Setup menu
-2. Select the Install Module button and enter the following URL: https://raw.githubusercontent.com/jopeek/fvtt-simple-dice-roller/master/module.json
-3. Click Install and wait for installation to complete 
-
-### Feedback
-
-If you have any suggestions or feedback, please contact me on Discord (ChalkOne#0156).
+*** UNDER CONSTRUCTION ***
---
+++ scripts/simple-dice-roller-hooks.js
@@ -0,0 +1,70 @@
+import { SimpleDiceRoller } from "../scripts/simple-dice-roller-main.js";
+
+// here is where we 'hook' to the event framework used by FoundryVTT
+// https://foundryvtt.com/api/classes/client.Hooks.html
+Hooks.once("init", () => {
+    _loadHandlebarTemplates();
+    _registerGameSettings();
+    console.log("SDR | Simple Dice Roller Deluxe is loaded");
+});
+
+// TODO P2: split static init from rendering table
+// Hooks.on("getSceneControlButtons", controls => {
+Hooks.on('renderSceneControls', (controls, html) => {
+    SimpleDiceRoller.Init(controls, html);
+});
+
+function _loadHandlebarTemplates() {
+   Handlebars.registerHelper('isD100', function (value) {
+       return value === 100;
+   });
+   Handlebars.registerHelper('isFate', function (value) {
+       return value === "f";
+   });
+   loadTemplates([SimpleDiceRoller.TEMPLATE_PATH]);
+}
+
+function _registerGameSettings() {
+    // TODO P3: look into localization options; best practices?
+    // TODO P4: update localization
+    game.settings.register(SimpleDiceRoller.ID, SimpleDiceRoller.CONFIG_MAXDICE_COUNT, {
+        name: game.i18n.localize("simpleDiceRoller.maxDiceCount.name"),
+        hint: game.i18n.localize("simpleDiceRoller.maxDiceCount.hint"),
+        scope: "world",
+        config: true,
+        default: 7,
+        type: Number
+    });
+    game.settings.register(SimpleDiceRoller.ID, SimpleDiceRoller.CONFIG_ENABLE_1ST_COLUMN, {
+        name: game.i18n.localize("simpleDiceRoller.enableFirstColumn.name"),
+        hint: game.i18n.localize("simpleDiceRoller.enableFirstColumn.hint"),
+        scope: "world",
+        config: true,
+        default: false,
+        type: Boolean
+    });
+    game.settings.register(SimpleDiceRoller.ID, SimpleDiceRoller.CONFIG_ENABLE_D2, {
+        name: game.i18n.localize("simpleDiceRoller.enableD2.name"),
+        hint: game.i18n.localize("simpleDiceRoller.enableD2.hint"),
+        scope: "world",
+        config: true,
+        default: false,
+        type: Boolean
+    });
+    game.settings.register(SimpleDiceRoller.ID, SimpleDiceRoller.CONFIG_ENABLE_D100, {
+        name: game.i18n.localize("simpleDiceRoller.enableD100.name"),
+        hint: game.i18n.localize("simpleDiceRoller.enableD100.hint"),
+        scope: "world",
+        config: true,
+        default: false,
+        type: Boolean
+    });
+    game.settings.register(SimpleDiceRoller.ID, SimpleDiceRoller.CONFIG_ENABLE_FUDGE, {
+        name: game.i18n.localize("simpleDiceRoller.enableFudgeDice.name"),
+        hint: game.i18n.localize("simpleDiceRoller.enableFudgeDice.hint"),
+        scope: "world",
+        config: true,
+        default: false,
+        type: Boolean
+    });
+}
